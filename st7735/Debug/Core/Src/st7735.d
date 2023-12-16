09f106c2bd35f67da0a3277da70753c0d7d93de6
Vladislav Alexandrov <ava015@campus.mephi.ru>
2023-12-16 13:04:33 +0300

    new project
    Signed-off-by: Vladislav Alexandrov <ava015@campus.mephi.ru>

---
+++ st7735/Debug/Core/Src/st7735.d
@@ -0,0 +1,4 @@
+Core/Src/st7735.o: ../Core/Src/st7735.c ../Core/Inc/st7735.h \
+ ../Core/Inc/st7735_reg.h
+../Core/Inc/st7735.h:
+../Core/Inc/st7735_reg.h:
---
+++ st7735/Debug/Core/Src/st7735_reg.d
@@ -0,0 +1,2 @@
+Core/Src/st7735_reg.o: ../Core/Src/st7735_reg.c ../Core/Inc/st7735_reg.h
+../Core/Inc/st7735_reg.h:
